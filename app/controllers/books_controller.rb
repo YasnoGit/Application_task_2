3c61aac3dc12cd1cee76bcddd89bd809894cea6a
EC2 Default User <132417648+YasnoGit@users.noreply.github.com>
2023-06-05 11:29:35 +0000

    [Finish]応用課題3[Start]応用課題4

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,6 +4,7 @@ before_action :correct_user, only: [:edit, :update, :destroy]
   def show
     @book = Book.find(params[:id])
     @user = @book.user
+    @book_comment = BookComment.new
   end
 
   def index
